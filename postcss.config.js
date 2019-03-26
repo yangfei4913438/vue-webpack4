acf234c041fdb6d328b99149af4adae89f60ae4e
yangfei <yangfei86@vip.qq.com>
2019-03-26 16:39:26 +0800

    autoprefixer 参数调整

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
-      require('autoprefixer'),
-      require('cssnano')
+    require('autoprefixer')({
+      browsers: ['> 1%', 'last 3 versions'] // 代表意思为每个主流浏览器的最后3个版本
+    }),
+    require('cssnano') // 压缩和优化css的插件
   ]
 };
