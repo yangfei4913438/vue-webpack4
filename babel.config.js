76e8ee0b595ffc00c18b0fa6f6d262c547c5d674
yangfei <yangfei86@vip.qq.com>
2019-04-03 18:24:04 +0800

    - 兼容性增强

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,8 @@ const presets = [
 ];
 
 const plugins = [
-  "@babel/plugin-syntax-dynamic-import"
+  "@babel/plugin-syntax-dynamic-import",
+  "@babel/plugin-transform-runtime"
 ];
 
 module.exports = {
