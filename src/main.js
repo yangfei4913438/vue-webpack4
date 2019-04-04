a3dad032da309371f15e49a8559ff495d7bf7a03
yangfei <yangfei86@vip.qq.com>
2019-04-04 09:38:50 +0800

    - 增加 vue 的默认配置。

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+Vue.config.productionTip = false;
+
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  components: {
+    App
+  },
+  template: '<App/>'
 });
